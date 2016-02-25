1b8e4238d835f4d9a5fecc6c9d2cfff9627e6c92
Hecham Ghazal <hecham.ghazal@gmail.com>
2016-02-25 10:33:26 -0500

    added testing framework

---
+++ tests/karma.conf.js
@@ -0,0 +1,82 @@
+import webpack from 'webpack';
+
+module.exports = function(config) {
+  config.set({
+    
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+    
+    // CircleCI sets the CI environment variable to true
+    // https://circleci.com/docs/environment-variables
+    singleRun: !!process.env.CI,
+
+    frameworks: [ 'mocha' ],
+    
+    files: [
+      './node_modules/phantomjs-polyfill/bind-polyfill.js',
+      '*.spec.js',
+      '**/*.spec.js',
+    ],
+
+    preprocessors: {
+      '*.spec.js': [ 'webpack', 'sourcemap' ],
+      '**/*.spec.js': [ 'webpack', 'sourcemap' ],
+    },
+
+    reporters: ['mocha'],
+
+    plugins: [
+      require('karma-webpack'),
+      require('karma-mocha'),
+      require('karma-mocha-reporter'),
+      require('karma-phantomjs-launcher'),
+      require('karma-sourcemap-loader'),
+    ],
+
+    webpack: {
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          { test: /\.(jpe?g|png|gif|svg)$/, loader: 'url', query: { limit: 10240 } },
+          { test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: ['babel'] },
+          { test: /\.json$/, loader: 'json-loader' },
+          { test: /\.scss$/, loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap' },
+        ],
+      },
+      resolve: {
+        modulesDirectories: [
+          '.',
+          'src',
+          'node_modules',
+        ],
+        extensions: ['', '.json', '.js', 'jsx'],
+      },
+      plugins: [
+        new webpack.IgnorePlugin(/\.json$/),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+          __DEV__: true,
+          __DEVTOOLS__: false,
+        }),
+      ],
+    },
+
+    webpackServer: {
+      noInfo: true,
+    },
+    
+    reportSlowerThan: 100,
+    
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+  });
+};
---
+++ tests/testFramework.spec.js
@@ -0,0 +1,35 @@
+/* eslint no-unused-vars:0 */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { renderIntoDocument } from 'react-addons-test-utils';
+import { expect, assert } from 'chai';
+import sinon from 'sinon';
+
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router';
+
+import Root from 'src/containers/Root.jsx';
+
+function once(fn) {
+  let returnValue, called = false;
+  return () => {
+    if (!called) {
+      called = true;
+      returnValue = fn.apply(this, arguments);
+    }
+    return returnValue;
+  };
+}
+
+describe('Check testing framework', () => {
+  it('should correctly import Chai', () =>   {
+    return expect(1).to.equal(1);
+  });
+  
+  it('should correctly import Sinon', () =>   {
+    const spy = sinon.spy();
+    const proxy = once(spy);
+    proxy();
+    assert(spy.called);
+  });
+});
